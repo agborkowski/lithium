0fa60dee29025420245393cbde2fe67dfb7414b5
Simon JAILLET <sjaillet@gmail.com>
2013-05-01 03:06:38 +0200

    Save relations implementation.

--- tests/cases/data/source/MongoDbTest.php
+++ tests/cases/data/source/MongoDbTest.php
@@ -444,6 +444,7 @@ class MongoDbTest extends \lithium\test\Unit {
 
 		$result = $this->_db->connection->queries;
 		$createOpts = array(
+			'with' => false,
 			'validate' => true,
 			'events' => 'create',
 			'whitelist' => null,
@@ -651,8 +652,8 @@ class MongoDbTest extends \lithium\test\Unit {
 			'collection' => 'posts',
 			'data' => array('initial' => 'one', 'values' => 'two', '_id' => $document->_id),
 			'options' => array(
-				'validate' => true, 'events' => 'create', 'whitelist' => null, 'callbacks' => true,
-				'locked' => false, 'safe' => false, 'fsync' => false
+				'with' => false, 'validate' => true, 'events' => 'create', 'whitelist' => null,
+				'callbacks' => true, 'locked' => false, 'safe' => false, 'fsync' => false
 			)
 		);
 		$this->assertEqual($expected, $result);
@@ -668,7 +669,7 @@ class MongoDbTest extends \lithium\test\Unit {
 			'conditions' => array('_id' => $document->_id),
 			'update' => array('$set' => array('values' => 'new')),
 			'options' => array(
-				'validate' => true, 'events' => 'update', 'whitelist' => null,
+				'with' => false, 'validate' => true, 'events' => 'update', 'whitelist' => null,
 				'callbacks' => true, 'locked' => false, 'upsert' => false, 'multiple' => true,
 				'safe' => false, 'fsync' => false
 			)
